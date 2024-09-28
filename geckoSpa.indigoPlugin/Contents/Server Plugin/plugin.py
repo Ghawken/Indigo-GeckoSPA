0906750eb9af94949e42991662359d5a14ea3f37
Ghawken <ghawken@hotkey.net.au>
2024-09-28 17:02:02 +1000

    0.1.44
    Fix for Eco SpaMode Valuemapping absence
    BREAKING CHANGE - will need child devices deleted and re-created (sorry!)
    Adds some more data to individual devices including key_pad, ui_keys etc. to enable updating
    Supports multiple child devices updating (not sure why would have more than one copy of same device)
    Some bug fixes
    
    0.1.35
    Update to device state handling.  Use Observers to immediately update states on Pumps, Lights, Blowers and EcoMode.
    With states of devices pending after button press for this update - so if State changed update has been actioned/completed successfully.
    
    Fix logging - self.debug (!) Removed and proper log and file logging selection
    
    0.1.32
    Quite a big bug fix for moving to full Library
    Adds Observers for all objects - logged.
    Change to Heater logic/Timer logic
    Add support for all Raw States in Raw states device states
    Update for Heater True/False,++
    
    0.1.21
    Bug fix for DeiceUpdate not being datetime
    
    0.1.20
    Indigo API 3.4
    Update Libraries
    Startup information

--- geckoSpa.indigoPlugin/Contents/Server Plugin/plugin.py
+++ geckoSpa.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -439,23 +439,15 @@ class pluginSpa(GeckoAsyncSpaMan, Thread):
     def on_eco_mode_changed(self, sender, old_value, new_value):
         _LOGGER.info(f"Eco Mode {sender.tag} changed from {old_value} to {new_value}")
         _LOGGER.debug(f"{sender}")
-        # Map new_value to boolean
-        value_mapping = {
-            'ON': True,
-            'OFF': False,
-            'HIGH': True,
-            'HI': True,
-            'LOW': True,  # Map 'LOW' to True as per your requirement
-        }
 
         # Get the boolean value, default to False if not found
-        boolean_value = value_mapping.get(new_value, False)
+        #  boolean_value = value_mapping.get(new_value, False)
 
         # Create an update message
         update = {
             'type': 'ecomode_changed',
             'device_number': 0,
-            'new_value': boolean_value,
+            'new_value': new_value,
         }
 
         # Enqueue the update
